5a9c227f95e57c079636d3ee0d15e8a6684d9302
Jordan Roberts <jrobertswebdev@gmail.com>
2023-12-14 17:01:44 -0600

    modify schema

--- index.js
+++ index.js
@@ -18,6 +18,38 @@ app.get('/users', async (req, res) => {
   }
 });
 
+
+app.post('/save', async (req, res) => {
+  try{
+    const {year, make, model, doors, class:truckClass , a, b, c } = req.body;
+   
+    console.log(req.body);
+    const newVehicle = await prisma.vehicle.create({
+      data: {
+        year: year,
+        make: make,
+        model: model,
+        doors: doors,
+        class: truckClass,
+        window: {
+          create: {
+            a: a,
+            b: b,
+            c: c,
+          }
+        }
+      },
+      include: {
+        window: true
+      }
+    });
+    res.json(newVehicle);
+
+  } catch (error) {
+    console.log(error);
+  }
+})
+
 app.post('/user', async (req, res) => {
     const { name, email } = req.body;
     try{
